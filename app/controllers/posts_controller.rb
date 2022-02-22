50363cf047a9291d43430888d5bac7a5ded05644
malikelarbi <malik.elarbi@kellogg.northwestern.edu>
2022-02-22 00:17:09 +0000

    committing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,18 @@
 class PostsController < ApplicationController
 
-    def index
-        @posts = Post.all
+    def show
+        @post = Post.find(params["id"])
+    end
+
+    def new
+        @post = Post.new
+        @post.place_id = params("place_id")
+    end
+
+    def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places/#{@contact.place_id}"
     end
 
 end
